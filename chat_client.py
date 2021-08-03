0e062e867cfc7cd1300461930ffc7b826564101a
Eric Chiu <ericwchiu@gmail.com>
2021-08-02 23:21:35 -0700

    chat works

--- chat_client.py
+++ chat_client.py
@@ -5,12 +5,27 @@
 import socket
 import sys
 
-# establishes server on specified host and port
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 addr = '127.0.0.1'
 port = 5000   
 
+# establishes server on specified host and port
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((addr, port))  
-print('Listening on ' + addr + ' port ' + str(port))
 
+while True:
+    msg = input('client: ')
+
+    client.send(msg.encode())
+
+    if '/q' in msg:
+        print('Terminating connection')
+        client.close()
+        exit()
+    else:
+        recv_msg = client.recv(2048).decode()
+        print(f'server: {recv_msg}')
+
+        if '/q' in recv_msg:
+            print('Terminating connection')
+            client.close()
+            exit()
--- chat_server.py
+++ chat_server.py
@@ -5,50 +5,55 @@
 import socket
 import sys
 
+# if len(sys.argv) < 2:
+#     print('Please rerun and specify a port!')
+#     exit()
+
+addr = '127.0.0.1'
+port = 5000 
+
 # establishes server on specified host and port
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-addr = '127.0.0.1'
-port = 5000  
 
 print('Listening on ' + addr + ' port ' + str(port))
 
 # create the server using the socket module
 server.bind((addr, port))
 server.listen(1)   
+
 conn, client_addr = server.accept()
 
 print(f'The client at {client_addr} has connected to the server')
-conn.send(f'Welcome to the jungle, {client_addr}')
+
+server.close()
 
 # listens for requests, prints any incoming messages, and sends a response
 while True:
-    recv_msg = conn.recv(2048)
+    recv_msg = conn.recv(2048).decode()
     
     # receive client msg
     if recv_msg:
-        print(f'{client_addr} >{recv_msg}')
+        print(f'client: {recv_msg}')
 
-        # if client sent the quit command
-        if recv_msg == '/q':
+        if '/q' in recv_msg:
             print('Thanks for stopping by! The connection is being terminated')
             conn.close()
             server.close()
             exit()
 
     # process and send msg from server
-    send_msg = input(f'{addr} >')
+    send_msg = input('server: ')
 
     while len(send_msg) < 1 or len(send_msg) > 200:
         print('Error! Your message must be between 1 and 200 characters')
-        send_msg = input(f'{addr} >')
+        send_msg = input('server: ')
 
-    if send_msg == '/q':
+    conn.send(send_msg.encode())
+
+    # check if we can quit
+    if '/q' in send_msg:
             print('Thanks for stopping by! The connection is being terminated')
             conn.close()
             server.close()
             exit()
-    else:
-        print(f'{addr} >')
-        conn.send(send_msg)
