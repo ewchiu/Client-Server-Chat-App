18a7e76f5090e76182a728c52f70d562788b46ed
Eric Chiu <ericwchiu@gmail.com>
2021-08-02 21:51:31 -0700

    implementing chat server

--- chat_server.py
+++ chat_server.py
@@ -3,22 +3,36 @@
 # Client-Server Chat Implementation
 
 import socket
+import select
 import sys
 
-host = str(sys.argv[1])
-port = str(sys.argv[2])                
+            
 
 # establishes server on specified host and port
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+if len(sys.argv) != 3:
+	print("Error - not enough arguments. IP address and port number needs to be included")
+	exit()
+
+addr = str(sys.argv[1])
+port = str(sys.argv[2])    
+
+print('Listening on ' + addr + ' port ' + str(port))
+print('Welcome to the chat room! To terminate the session, type /q into the chat')
+
+# create the server using the socket module
+server.bind((addr, port))
 server.listen(1)   
-print('Listening on ' + host + ' port ' + str(port))
+conn, client_addr = server.accept()
+
+print(f'The client at {client_addr} has connected to the server')
 
 # listens for requests, prints any incoming messages, and sends a response
 while True:
-    conn, addr = server.accept()
-    msg = conn.recv(1024)
-    print(msg)
+    recv_msg = conn.recv(2048)
+    
+    if recv_msg:
+        print(f'<{client_addr}> {recv_msg}')
 
-
-    conn.close()    # terminates socket connection
